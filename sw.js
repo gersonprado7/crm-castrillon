8f10ba9453403bc873fc75ef2da8b26c1193c9cb
gersonprado7 <gerson.prado@castrillon.com.br>
2026-06-07 18:08:31 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'castrillon-crm-v2';
+const FILES = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return fetch(e.request).then(fresh => {
+        caches.open(CACHE).then(c => c.put(e.request, fresh.clone()));
+        return fresh;
+      }).catch(() => cached);
+    })
+  );
+});
